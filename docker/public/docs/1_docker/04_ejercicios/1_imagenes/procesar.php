d4eaa26a46a10dde5bb4ee70b80190f50da95b6c
MAlejandroR <manuelromeromiguel@gmail.com>
2024-09-26 15:31:46 +0200

    trabajando hugo

--- docker/public/docs/1_docker/04_ejercicios/1_imagenes/procesar.php
+++ docker/public/docs/1_docker/04_ejercicios/1_imagenes/procesar.php
@@ -15,16 +15,12 @@ function contains(string|null $respuesta, string|null $solucion): bool
 }
 
 $respuesta = htmlspecialchars(filter_input(INPUT_POST, "respuesta"));
-
 $respuesta = trim(preg_replace('/\s+/', ' ', $respuesta));$solucion = htmlspecialchars(filter_input(INPUT_POST, "solucion"));
 
 $ejercicio = filter_input(INPUT_POST, "ejercicio");
 
 $rtdo = contains($respuesta, $solucion);
-/*Por si lo preparo como un api*/
-//$respuesta=['respueta'=>$respuesta, 'solucion'=>$solucion, 'resultado'=>$rtdo];
-//$respuesta =json_encode($respuesta);
-//return $respuesta;
+
 $msj = $rtdo ? "<span style='color:green'>Muy bien</span>" : "<span style='color:red'>No parece una buena solución</span>";
 
 $msj .= "<br />Respuesta <span style='color:green'>$respuesta</span>";
@@ -36,62 +32,60 @@ header("Refresh: 5; url=$paginaDeReferencia");
 
 ?>
 
-
 <!doctype html>
 <html itemscope itemtype="http://schema.org/WebPage" lang="es" class="no-js">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="alternate" type="application/rss&#43;xml" href="http://web.infenlaces.com/dwes/docker/docs/1_docker/04_ejercicios/1_imagenes/index.xml">
+    <meta name="generator" content="Hugo 0.118.2">
+    <link rel="alternate" type="application/rss&#43;xml"
+          href="http://proyectosdwa.es/manuel/docs/0_docker/1_ejercicios/1_imagenes/index.xml">
     <meta name="robots" content="index, follow">
 
 
-    <link rel="shortcut icon" href="/dwes/docker/favicons/favicon.ico" >
-    <link rel="apple-touch-icon" href="/dwes/docker/favicons/apple-touch-icon-180x180.png" sizes="180x180">
-    <link rel="icon" type="image/png" href="/dwes/docker/favicons/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="/dwes/docker/favicons/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/dwes/docker/favicons/android-36x36.png" sizes="36x36">
-    <link rel="icon" type="image/png" href="/dwes/docker/favicons/android-48x48.png" sizes="48x48">
-    <link rel="icon" type="image/png" href="/dwes/docker/favicons/android-72x72.png" sizes="72x72">
-    <link rel="icon" type="image/png" href="/dwes/docker/favicons/android-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="/dwes/docker/favicons/android-144x144.png" sizes="144x144">
-    <link rel="icon" type="image/png" href="/dwes/docker/favicons/android-192x192.png" sizes="192x192">
+    <link rel="shortcut icon" href="/manuel/favicons/favicon.ico">
+    <link rel="apple-touch-icon" href="/manuel/favicons/apple-touch-icon-180x180.png" sizes="180x180">
+    <link rel="icon" type="image/png" href="/manuel/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/manuel/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/manuel/favicons/android-36x36.png" sizes="36x36">
+    <link rel="icon" type="image/png" href="/manuel/favicons/android-48x48.png" sizes="48x48">
+    <link rel="icon" type="image/png" href="/manuel/favicons/android-72x72.png" sizes="72x72">
+    <link rel="icon" type="image/png" href="/manuel/favicons/android-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/manuel/favicons/android-144x144.png" sizes="144x144">
+    <link rel="icon" type="image/png" href="/manuel/favicons/android-192x192.png" sizes="192x192">
 
-    <title>Crea imágenes | Desarrollo Web</title>
-    <meta name="description" content="Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins.">
-    <meta property="og:url" content="http://web.infenlaces.com/dwes/docker/docs/1_docker/04_ejercicios/1_imagenes/">
-    <meta property="og:site_name" content="Desarrollo Web">
-    <meta property="og:title" content="Crea imágenes">
-    <meta property="og:description" content="Descargar, ver y borrar imágenes Comandos a usar docker pull docker images docker rmi nombre_imagen docker rmi (docker images) 1. Descarga una imagen llamada ubuntu_latest 2. Descarga una imagen llamada manolo/web:v1 3. Verficia que las has descargado listándolas 4. Borra la imagen llamada manolo/web:v10 5. Verifica que la has borrado 6. Vuelve a cargar una imagen llamada manolo/web:v10 7. Borra todas las imágemes 8. Verifica que no tienes ninguna imagen ">
-    <meta property="og:locale" content="es">
-    <meta property="og:type" content="website">
-
+    <title>Soluciones a ejercicios</title>
+    <meta name="description" content="Apuntes sobre web">
+    <meta property="og:title" content="Crea imágenes"/>
+    <meta property="og:description" content="Apuntes sobre web"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="http://proyectosdwa.es/manuel/docs/0_docker/1_ejercicios/1_imagenes/"/>
     <meta itemprop="name" content="Crea imágenes">
-    <meta itemprop="description" content="Descargar, ver y borrar imágenes Comandos a usar docker pull docker images docker rmi nombre_imagen docker rmi (docker images) 1. Descarga una imagen llamada ubuntu_latest 2. Descarga una imagen llamada manolo/web:v1 3. Verficia que las has descargado listándolas 4. Borra la imagen llamada manolo/web:v10 5. Verifica que la has borrado 6. Vuelve a cargar una imagen llamada manolo/web:v10 7. Borra todas las imágemes 8. Verifica que no tienes ninguna imagen ">
-    <meta itemprop="wordCount" content="70">
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:title" content="Crea imágenes">
-    <meta name="twitter:description" content="Descargar, ver y borrar imágenes Comandos a usar docker pull docker images docker rmi nombre_imagen docker rmi (docker images) 1. Descarga una imagen llamada ubuntu_latest 2. Descarga una imagen llamada manolo/web:v1 3. Verficia que las has descargado listándolas 4. Borra la imagen llamada manolo/web:v10 5. Verifica que la has borrado 6. Vuelve a cargar una imagen llamada manolo/web:v10 7. Borra todas las imágemes 8. Verifica que no tienes ninguna imagen ">
+    <meta itemprop="description" content="Apuntes sobre web">
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:title" content="Crea imágenes"/>
+    <meta name="twitter:description" content="Apuntes sobre web"/>
 
 
-
-
-    <link rel="preload" href="/dwes/docker/scss/main.min.a18241fb76e390a924e57554186f3b012335c9bde30f23936d3bd6d9b05f0f6e.css" as="style">
-    <link href="/dwes/docker/scss/main.min.a18241fb76e390a924e57554186f3b012335c9bde30f23936d3bd6d9b05f0f6e.css" rel="stylesheet" integrity="">
+    <link rel="preload"
+          href="/manuel/scss/main.min.a259b9314771009688f4539373ec884b9c75fd106d86fce70f6e126c27b44f1a.css" as="style">
+    <link href="/manuel/scss/main.min.a259b9314771009688f4539373ec884b9c75fd106d86fce70f6e126c27b44f1a.css"
+          rel="stylesheet" integrity="">
 
     <script
-        src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-        crossorigin="anonymous"></script>
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha384-vtXRMe3mGCbOeY7l30aIg8H9p3GdeSe4IFlP6G8JMa7o7lXvnz3GFKzPxzJdPfGK"
+            crossorigin="anonymous"></script>
 
 </head>
 <body class="td-section">
 <header>
     <header>
         <nav class="js-navbar-scroll navbar navbar-expand navbar-dark flex-column flex-md-row td-navbar">
-            <a class="navbar-brand" href="/manuel/"><span class="navbar-brand__logo navbar-logo">Desarrollo Web</span></a>
+            <a class="navbar-brand" href="/manuel/"><span class="navbar-brand__logo navbar-logo"></span><span
+                        class="navbar-brand__name">Desarrollo Web</span></a>
             <div class="td-navbar-nav-scroll ml-md-auto pr-20%" id="main_navbar ">
-                <h1 style="text-color:white">Soluciones a ejercicos</h1>
+                <h1>Soluciones a ejercicos</h1>
                 </ul>
             </div>
             <div class="navbar-nav d-none d-lg-block">
@@ -105,9 +99,12 @@ header("Refresh: 5; url=$paginaDeReferencia");
     <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
         <div class="text-center p-4" style="border: 2px solid #ccc; background-color: #f9f9f9;">
             <h1><?= $msj ?></h1>
-            <hr />
-            <span class="d-block text-end fw-bold fst-italic" style="font-size: 1.2rem;">En 5 segundos la página te redirigirá....</span>
+            <div class="text-right font-weight-bold">
+                En 5 segundos la página se redirigirá.
+            </div>
         </div>
     </div>
+
+
     </body>
     </html>
